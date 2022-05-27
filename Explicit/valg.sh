3f557a8066ac62b73a7d1d3b7db50c5a99403467
Francis FENG <1017426291@qq.com>
2022-05-28 06:06:43 +0800

    Update valg.sh
    修正了valgrind方法验证内存泄漏

--- Explicit/valg.sh
+++ Explicit/valg.sh
@@ -9,6 +9,6 @@ module load intel/2018.4
 module load mpi/intel/2018.4
 module load valgrind/3.14.0
 
-valgrind --tool=memcheck --leak-check=full \
+valgrind mpirun --tool=memcheck --leak-check=full \
   --show-leak-kinds=all --undef-value-errors=no \
   ./explicit.out > $LSB_JOBID.log 2>&1
