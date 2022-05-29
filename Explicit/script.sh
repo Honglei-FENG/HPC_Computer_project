f47a448c6b831f5e7431081a52201e30877cc7d0
Francis FENG <1017426291@qq.com>
2022-05-29 12:00:20 +0800

    Update script.sh
    让sh文件变得简洁

--- Explicit/script.sh
+++ Explicit/script.sh
@@ -2,20 +2,10 @@
 #BSUB -J petsc-problem1
 #BSUB -q ser
 #BSUB -n 1
-#BSUB -e %J-petsc.err
-#BSUB -o %J-petsc.out
+
 
 module purge
 module load intel/2018.4
 module load mpi/intel/2018.4
 
-mpirun -np 1 ./explicit.out -ksp_type gmres \
-  -ksp_gmres_restart 30 -ksp_rtol 1.0e-10 \
-  -ksp_atol 1.0e-50 -ksp_max_it 1500 \
-  -ksp_gmres_modifiedgramschmidt \
-  -pc_type asm \
-  -sub_ksp_type richardson \
-  -sub_pc_type icc -ksp_monitor_short \
-  -ksp_converged_reason \
-  -ksp_view \
-  -log_view > $LSB_JOBID.log 2>&1
+mpirun -np 1 ./explicit.out > $LSB_JOBID.log 2>&1
