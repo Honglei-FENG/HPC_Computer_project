129dd4301f0ce122c38c6022a84222554557743e
Francis FENG <1017426291@qq.com>
2022-05-31 15:21:13 +0800

    Update script.sh
    设置求解器参数

--- Implicit/script.sh
+++ Implicit/script.sh
@@ -8,4 +8,12 @@ module purge
 module load intel/2018.4
 module load mpi/intel/2018.4
 
-/usr/bin/time -f "run time is: %e" mpirun -np 1 ./implicit.out > $LSB_JOBID.log 2>&1
+/usr/bin/time -f "run time is: %e" mpirun -np 1 ./implicit.out -ksp_type gmres \
+  -ksp_gmres_restart 30 -ksp_rtol 1.0e-10 \
+  -ksp_atol 1.0e-50 -ksp_max_it 1500 \
+  -ksp_gmres_modifiedgramschmidt \
+  -pc_type jacobi \
+  -sub_ksp_type richardson \
+  -sub_pc_type icc \
+  -ksp_converged_reason> $LSB_JOBID.log 2>&1
+
